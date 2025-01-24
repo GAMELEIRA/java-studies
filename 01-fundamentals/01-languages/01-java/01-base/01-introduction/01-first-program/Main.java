96f4d1a8d551cb8acfbda47b04984c4d2dd86424
GAMELEIRA <gameleira270499@gmail.com>
2025-01-24 17:35:55 -0300

    feat(first-program): Adiciona comentario de aprendizado com o primeiro programa java

--- 01-fundamentals/01-languages/01-java/01-base/01-introduction/01-first-program/Main.java
+++ 01-fundamentals/01-languages/01-java/01-base/01-introduction/01-first-program/Main.java
@@ -1,5 +1,10 @@
+//TODO PROGRAMA EM JAVA DEVE ESTAR CONTIDO EM UMA CLASSE
+//TODA CLASSE DEVE TER A PRIMEIRA LETRA MAISCULA
+//O NOME DA CLASSE DEVE SER IGUAL AO NOME DO ARQUIVO
 public class Main {
+    //O METODO main É PRINCIPAL DO PROGRAMA
     public static void main(String[] args) {
+        //IMPRIME UMA FRASE NO TERMINAL
         System.out.println("Hello World!");
     }
 }
