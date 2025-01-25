0709562261d12b2b109c0e92c9287101c8a93f6b
GAMELEIRA <gameleira270499@gmail.com>
2025-01-25 15:50:17 -0300

    feat(booleans): Adiciona aprendizado com tipos booleanos

--- 01-fundamentals/01-languages/01-java/01-base/01-introduction/05-data-types/Main.java
+++ 01-fundamentals/01-languages/01-java/01-base/01-introduction/05-data-types/Main.java
@@ -13,22 +13,43 @@ public class Main {
     //short    | 16 bits, NUMEROS DE -32.768 ATE 32.767
     //int      | 32 bits, NUMEROS DE -2.147.483.648 ATE 2.147.483.647
     //long     | 64 bits, NUMEROS DE -9.223.372.036.854.775.808 ATE 9.223.372.036.854.775.807
-    //float    | 32 bits, NUMEROS DE 3.4 x 10^-38 ATE 3.4 x 10^38
-    //double   | 64 bits, NUMEROS DE 1.7 x 10^-308 ATE 1.7 x 10^308
+    //float    | 32 bits, NUMEROS DE 3.4 x 10^-38 ATE 3.4 x 10^38, PODE CONTER ATE 7 DIGITOS DECIMAIS
+    //double   | 64 bits, NUMEROS DE 1.7 x 10^-308 ATE 1.7 x 10^308, PODE CONTER ATE 16 DIGITOS DECIMAIS
     //char     | 16 bits, CADEIA DE CARACTERES 0 ATE 65.535, Unicode 16-bit code unit
     //boolean  | VALORES BOOLEANOS true or false
     //String   | SEQUENCIA DE CARACTERES
     //=======================================================================
 
+    //OS NUMEROS PRIMITIVOS SAO DIVIDIDOS EM DOIS GRUPOS
+    //NUMEROS INTEIROS: byte, short, int e long
+    //NUMEROS FLUTUANTES: float e double
+    
     byte myByte = 127;
     short myShort = 32767;
     int myInt = 2147483647;
     long myLong = 9223372036854775807L;
     float myFloat = 3.4E38f;
-    double myDouble = 1.7;
+    double myDouble = 1.7d;
+    
+    System.out.println("byte: " + myByte);
+    System.out.println("short: " + myShort);
+    System.out.println("int: " + myInt);
+    System.out.println("long: " + myLong);
+    System.out.println("float: " + myFloat);
+    System.out.println("double: " + myDouble);
+
+
+
     char myChar = 'a';
-    boolean myBoolean = true;
     String myString = "Gabriel Gameleira dos Santos";
+
+    System.out.println("char: " + myChar);
+    System.out.println("String: " + myString);
+
+    //VALORES BOOLEANOS REPRESENTAM SIM OU NAO / VERDADEIRO OU FALSO / 0 OU 1
+    //ESSE TIPO DE VALOR TAMBEM PODE SER OBTIDO POR MEIO DE CONDICIONAIS. EXEMPLO 10 == 10 (IGUAL A TRUE)
+    boolean myBoolean = true;
+    System.out.println("boolean: " + myBoolean);
 
   }  
 }
