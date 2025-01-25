419a128ab70d0b8af0f65d4444901ee63a611bea
GAMELEIRA <gameleira270499@gmail.com>
2025-01-25 02:01:10 -0300

    feat(variables-introduction): Adiciona introducao a declaracao de variaveis

---
+++ 01-fundamentals/01-languages/01-java/01-base/01-introduction/04-variables/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+  public static void main(String[] args) {
+    //VARIAVEIS SAO CONTAINERS PARA ARMAZENAR VALORES DE DADOS
+    //EXISTEM TIPOS DIFERENTES DE VARIAVEIS EXEMPLO: String, char, int, float, double, boolean
+
+    //PARA CRIAR UMA VARIAVEL VOCE DEVE ESPECIFICAR O TIPO E/OU ATRIBUIR UM VALOR A ELA
+    //tipo nomeDaVariavel = value
+    //EXEMPLO
+    String name = "John";
+    System.out.println(name);
+
+    int myNum = 25;
+    System.out.println(myNum);
+    
+    int otherNum;
+    otherNum = 30;
+    System.out.println(otherNum);
+
+
+    //PARA DEFINIR UMA VARIAVEL CONSTANTE USO A PALAVRA-CHAVE final ANTES DO TIPO DA VARIAVEL
+    //EXEMPLO
+    final double PI = 3.14;
+    System.out.println(PI);
+
+  }
+}
