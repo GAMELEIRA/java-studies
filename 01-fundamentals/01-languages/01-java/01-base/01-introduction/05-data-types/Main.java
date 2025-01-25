731e454a23849a6df32495a0aebbb32301ab8afc
GAMELEIRA <gameleira270499@gmail.com>
2025-01-25 15:06:43 -0300

    feat(data-primitives): Adiciona aprendizado com dados primitivos

---
+++ 01-fundamentals/01-languages/01-java/01-base/01-introduction/05-data-types/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+  public static void main(String[] args) {
+    //TODA VARIAVEL PRECISA SER TIPADA, ASSIM COMO RETORNO DE FUNCOES.
+    //EM JAVA EXISTEM OS TIPOS PRIMITIVOS E OS TIPOS POR REFERENCIA
+    //TIPOS PRIMITIVOS: byte, short, int, long, float, double
+    //TIPOS POR REFERENCIA: String
+    //OS TIPOS DEFINEM O TIPO DA VARIAVEL E QUAIS VALORES ELA PODE CONTER
+
+
+    //=======================================================================
+    //DATA TYPE | DESCRIPTION 
+    //byte     | 8 bits, NUMEROS DE -128 ATE 127
+    //short    | 16 bits, NUMEROS DE -32.768 ATE 32.767
+    //int      | 32 bits, NUMEROS DE -2.147.483.648 ATE 2.147.483.647
+    //long     | 64 bits, NUMEROS DE -9.223.372.036.854.775.808 ATE 9.223.372.036.854.775.807
+    //float    | 32 bits, NUMEROS DE 3.4 x 10^-38 ATE 3.4 x 10^38
+    //double   | 64 bits, NUMEROS DE 1.7 x 10^-308 ATE 1.7 x 10^308
+    //char     | 16 bits, CADEIA DE CARACTERES 0 ATE 65.535, Unicode 16-bit code unit
+    //boolean  | VALORES BOOLEANOS true or false
+    //String   | SEQUENCIA DE CARACTERES
+    //=======================================================================
+
+    byte myByte = 127;
+    short myShort = 32767;
+    int myInt = 2147483647;
+    long myLong = 9223372036854775807L;
+    float myFloat = 3.4E38f;
+    double myDouble = 1.7;
+    char myChar = 'a';
+    boolean myBoolean = true;
+    String myString = "Gabriel Gameleira dos Santos";
+
+  }  
+}
