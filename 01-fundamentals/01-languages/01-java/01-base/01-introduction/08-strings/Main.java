8dd5f0e72405c6242d4f69874417a9c601ac9f97
GAMELEIRA <gameleira270499@gmail.com>
2025-01-26 15:38:50 -0300

    feat(strings): Adiciona aprendizado com manipulacao de strings

---
+++ 01-fundamentals/01-languages/01-java/01-base/01-introduction/08-strings/Main.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    //VARIAVEIS Strings SAO UTILIZADAS PARA ARMAZENAR TEXTO
+    //UMA String CONTEM UMA COLECAO DE CARACTERES ENTRE ASPAS DUPLAS
+    //UMA String É UM TIPO POR REFERENCIA QUE CONTEM METODOS QUE SAO UTILIZADAS PARA MANIPULACAO
+    String name = "Gabriel";
+    String middleName = "Gameleira";
+    System.out.println(name + " " + middleName);
+
+    //É POSSIVEL ENCONTRAR O TAMANHO DE UMA STRING USANDO O METODO length
+    System.out.println("Tamanho da string: " + name.length());
+  
+    //É POSSIVEL CONCATENAR DUAS STRINGS USANDO O OPERADOR +
+    String fullName = name + " " + middleName;
+    System.out.println("Nome completo: " + fullName);
+
+    //É POSSIVEL USAR O MÉTODO toUpperCase() PARA CONVERTER TODAS AS LETRAS EM MAISCULAS
+    String upperCaseName = name.toUpperCase();
+    System.out.println("Nome em maiúsculas: " + upperCaseName);
+
+    //É POSSIVEL USAR O MÉTODO toLowerCase() PARA CONVERTER TODAS AS LETRAS EM MINUSCULAS
+    String lowerCaseName = name.toLowerCase();
+    System.out.println("Nome em minúsculas: " + lowerCaseName);
+
+    //É POSSIVEL USAR O MÉTODO substring() PARA EXTRAIR UMA SUBSTRING DE UMA STRING
+    //O PRIMEIRO ARGUMENTO É O INDICE DA PRIMEIRA LETRA DA SUBSTRING
+    //O SEGUNDO ARGUMENTO É O INDICE DA PRIMEIRA LETRA DA SUBSTRING QUE NÃO VAI SER INCLUIDA
+    String subString = fullName.substring(0, 7);
+    System.out.println("Full name: "+ fullName + ". Substring: " + subString);
+
+    //É POSSIVEL USAR O MÉTODO indexOf() PARA LOCALIZAR O INDICE DA PRIMEIRA OCORRENNCIA DE UM CARACTERE
+    //O PRIMEIRO ARGUMENTO É O CARACTERE A SER LOCALIZADO
+    //O SEGUNDO ARGUMENTO É O INDICE EM QUE O CARACTERE SERA LOCALIZADO (OPCIONAL, SE NÃO INFORMADO, O MÉTODO IRA LOCALIZAR O PRIMEIRO ENCONTRADO)
+    int indexOfName = fullName.indexOf(name);
+    System.out.println("Full name: "+ fullName + ". Indice do nome: " + indexOfName);
+
+    //É POSSIVEL USAR O MÉTODO replace() PARA SUBSTITUIR UM CARACTERE POR OUTRO EM UMA STRING
+    //O PRIMEIRO ARGUMENTO É O CARACTERE A SER REMOVIDO
+    //O SEGUNDO ARGUMENTO É O CARACTERE A SER INSERIDO NA LOCALIZAÇÃO DO CARACTERE REMOVIDO
+    String replacedString = fullName.replace("Gabriel", "João");
+    System.out.println("Full name: "+ fullName + ". String substituída: " + replacedString);
+
+    //É POSSIVEL USAR O MÉTODO indexOf() PARA RETORNAR O INDICE(POSICAO) DA PRIMEIRA OCORRENCIA ESPECIFICADA NO TEXTO DE UMA STRING
+    //O PRIMEIRO ARGUMENTO É A STRING EM QUE SERA LOCALIZADO
+    //O SEGUNDO ARGUMENTO É O CARACTERE QUE SERA LOCALIZADO
+    //O TERCEIRO ARGUMENTO É O INDICE EM QUE O CARACTERE SERA LOCALIZADO (OPCIONAL, SE NÃO INFORMADO, O MÉTODO IRA LOCALIZAR O PRIMEIRO ENCONTRADO)
+    int indexOfCharacter = fullName.indexOf('e', 6);
+    System.out.println("Full name: "+ fullName + ". Indice do primeiro 'e' a partir da posição 6: " + indexOfCharacter);
+
+    //É POSSIVEL USAR O MÉTODO split() PARA DIVIDIR UMA STRING EM UM ARRAY DE SUBSTRINGS
+    //O ARGUMENTO É O CARACTERE USADO COMO SEPARADOR (OPCIONAL, SE NÃO INFORMADO, O MÉTODO USARA ESPAÇO EM BRANCO COMO SEPARADOR)
+    String[] fullNameParts = fullName.split(" ");
+    System.out.println("Full name: "+ fullName + ". Array de partes: " + Arrays.toString(fullNameParts));
+
+    //É POSSIVEL USAR O MÉTODO equals() PARA COMPARAR SE UMA STRING É IGUAL A OUTRA
+    //O ARGUMENTO É A STRING QUE SERA COMPARADA
+    boolean areEqual = fullName.equals("Gabriel Gameleira");
+    System.out.println("Full name: "+ fullName + ". Strings iguais: " + areEqual);
+
+  }
+}
