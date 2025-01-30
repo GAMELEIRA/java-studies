32b9639dd315c81f5035ec50f2233ee53b4f9d40
GAMELEIRA <gameleira270499@gmail.com>
2025-01-30 20:53:56 -0300

    feat(returns): Adiciona aprendizado com retorno de metodos em Java

--- 01-fundamentals/01-languages/01-java/01-base/02-methods/03-returns/Main.java
+++ 01-fundamentals/01-languages/01-java/01-base/02-methods/03-returns/Main.java
@@ -1,5 +1,13 @@
 public class Main {
   public static void main(String[] args) {
     //AS FUNCOES PODEM RETORNAR VALORES DO TIPO PRIMITIVO OU TIPO POR REFERENCIA
+    int result = sum(5, 3);
+    System.out.println("Soma: " + result);
   }  
+
+  //ESSA ASSINATURA DEFINE QUE SER RETORNADO UM VALOR NUMERICO INTEIRO AO FINALIZAR O BLOCO DE CODIGO
+  public static int sum(int a, int b) {
+    //return É UMA PALAVRA RESERVADA UTILIZADA PARA DEFINIR O RETORNO QUE IRA RETORNAR PELA FUNCAO
+    return a + b;
+  }
 }
