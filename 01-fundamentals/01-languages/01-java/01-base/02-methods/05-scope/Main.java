e499f09bd3efc42c50c6f7b07035dcc4666091a8
GAMELEIRA <gameleira270499@gmail.com>
2025-01-31 14:58:34 -0300

    feat(modifiers): Adiciona aprendizado com modificadores de acesso para funcoes em Java

---
+++ 01-fundamentals/01-languages/01-java/01-base/02-methods/05-scope/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+  //OS METODOS PODEM SER DEFINIDOS COMO public, protected e private. 
+  //DESSA FORMA É DEFINIDO A ACESSIBILIDADE DO METODO FORA DA CLASSE
+
+  //public PODE SER ACESSADO POR QUALQUER CLASSE
+  public static void main(String[] args) {
+    
+  }
+
+  //protected PODE SER ACESSADO POR QUALQUER CLASSE QUE ESTA NO MESMO PACOTE OU SUBCLASSE
+  protected void printMessage() {
+    System.out.println("Hello World!");
+  }
+
+  //private PODE SER ACESSADO APENAS NA CLASSE EM QUE FOI DEFINIDO
+  private void secretMessage() {
+    System.out.println("Vou te contar um segredo!");
+  }
+}
