7ce48a0c10d48f1b714a6823e9bb840271016862
GAMELEIRA <gameleira270499@gmail.com>
2025-01-31 16:16:12 -0300

    feat(recursion): Adiciona aprendizado com recursividade

---
+++ 01-fundamentals/01-languages/01-java/01-base/05-reference/01-recursion/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    //RECURSAO É A TECNICA DE FAZER UMA FUNCAO CHAMAR A SI MESMA
+    //COM O OBJETIVO DE RESOLVER UM PROBLEMA QUEBRANDO EM SUBPROBLEMAS MENORES
+    int result = sum(40);
+    System.out.println("Resultado: " + result);
+  }
+
+  public static int sum(int num) {
+    if (num > 0) {
+      System.out.println("Pilha:" + num);
+      return num + sum(num - 10);
+    } else {
+      return 0;
+    }
+  }
+}
