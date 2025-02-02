02bf373c60b538f79f8ffdfd275b4b180587df18
GAMELEIRA <gameleira270499@gmail.com>
2025-02-02 00:37:57 -0300

    feat(methods): Adiciona aprendizado com declaracao de metodos em Java

---
+++ 01-fundamentals/01-languages/01-java/01-base/03-oo/03-methods/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+  String name = "Gabriel";
+  
+  //METODOS EXECUTAM FUNCOES DENTRO DE UMA CLASSE
+  //METODOS SAO COMPOSTOS POR MODIFICADORES DE ACESSO, TIPO DE RETORNO E IDENTIFICADOR
+  public String sayHello() {
+    return "Olá, meu nome é " + this.name;
+  }
+}
