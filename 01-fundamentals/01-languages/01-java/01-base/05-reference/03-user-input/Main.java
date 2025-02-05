3a0ed8085bdf1ef51b0d0313435c58b89d885af8
GAMELEIRA <gameleira270499@gmail.com>
2025-02-05 01:27:40 -0300

    feat(input-user): Adiciona aprendizado com entrada de dados do usuário

---
+++ 01-fundamentals/01-languages/01-java/01-base/05-reference/03-user-input/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    //PARA ADICIONAR UMA ENTRADA DO USUARIO DEVE SER UTILIZADO A CLASSE Scanner
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Informe o seu nome: ");
+    String name = scanner.nextLine(); //PARA STRINGS
+
+    System.out.print("Informe sua idade: ");
+    byte age = scanner.nextByte(); //PARA BYTES
+
+    System.out.print("Você trabalha? ");
+    boolean isWork = scanner.nextBoolean(); //PARA BOOLEANOS
+
+    System.out.print("Qual seu salario dos sonhos? ");
+    double dreamSalary = scanner.nextDouble(); //PARA FLUANTES
+
+    System.out.println("Qual seu salário atual? ");
+    float actuallySalary = scanner.nextFloat(); //PARA FLUANTES
+
+    System.out.println("Quantas vezes você acha que já cortou o cabelo na vida? ");
+    int haircuts = scanner.nextInt(); //PARA INTEIROS
+
+    System.out.println("Quantas horas vividas você têm? ");
+    long hoursLived = scanner.nextLong(); //PARA INTEIROS
+    
+    System.out.println("Quantos dias vividos você têm? ");
+    short daysLived = scanner.nextShort(); //PARA INTEIROS LONGOS
+    
+    scanner.close(); //FECHA O SCANNER PARA LIBERAR RECURSOS
+
+    System.out.println("Nome: " + name + ", Idade: " + age + ", Trabalhando: " + isWork + ", Salario dos Sonhos: " + dreamSalary + ", Salario atual: " + actuallySalary + ", Quantidade de Cortes de Cabelo: " + haircuts + ", Quantidade de Horas Vividas: " + hoursLived + ", Dias vividos: " + daysLived );
+
+  }
+}
