4b14d178d0ee27ae21a7cdb07a776f2db3800036
GAMELEIRA <gameleira270499@gmail.com>
2025-02-06 14:46:29 -0300

    feat(collection): Adiciona aprendizado de como inverter a lista usando collections em Java

--- 01-fundamentals/01-languages/01-java/01-base/05-references/05-colletions/Main.java
+++ 01-fundamentals/01-languages/01-java/01-base/05-references/05-colletions/Main.java
@@ -12,11 +12,19 @@ public class Main {
     myNumbers.add(8);
     myNumbers.add(12);
 
-    //USAR sort PARA ORDENAR OS NUMEROS
-    Collections.sort(myNumbers); 
+    // USAR sort PARA ORDENAR OS NUMEROS
+    Collections.sort(myNumbers);
 
     for (int i : myNumbers) {
       System.out.println(i);
     }
+
+    // USAR Collections.reverseOrder() PARA ORDENAR EM ORDEM DECRESCENTE
+    Collections.sort(myNumbers, Collections.reverseOrder());
+
+    for (int i : myNumbers) {
+      System.out.println(i);
+    }
+
   }
 }
