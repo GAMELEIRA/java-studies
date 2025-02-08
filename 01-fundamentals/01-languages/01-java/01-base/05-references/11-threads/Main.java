9782518247b22be11698b81df016f3f03e633e6a
GAMELEIRA <gameleira270499@gmail.com>
2025-02-08 17:33:42 -0300

    feat(threads): Adiciona aprendizado com threads

--- 01-fundamentals/01-languages/01-java/01-base/05-references/11-threads/Main.java
+++ 01-fundamentals/01-languages/01-java/01-base/05-references/11-threads/Main.java
@@ -1,29 +1,34 @@
 public class Main extends Thread {
   public static void main(String[] args) {
-    //Threads PERMITEM QUE UM PROGRAMA OPERE DE FORMA MAIS EFICIENTE AO FAZER VARIAS COISAS AO MESMO TEMPO
-    //COM ISSO PODEM SER USADOS PARA EXECUTAR TAREFAS COMPLICADAS EM SEGUNDO PLANO SEM INTERROMPER O PROGRAMA PRINCIPAL
+    // Threads PERMITEM QUE UM PROGRAMA OPERE DE FORMA MAIS EFICIENTE AO FAZER
+    // VARIAS COISAS AO MESMO TEMPO
+    // COM ISSO PODEM SER USADOS PARA EXECUTAR TAREFAS COMPLICADAS EM SEGUNDO PLANO
+    // SEM INTERROMPER O PROGRAMA PRINCIPAL
     Main thread = new Main();
     thread.start();
     System.out.println("Codigo rodando fora da thread");
-
+    //VALIDA SE A THREAD TERMINOU ANTES DE USAR QUAISQUER ATRIBUTOS QUE A THREAD POSSA ALTERAR
+    while (thread.isAlive()) {
+      System.out.println("Waiting...");
+    }
   }
 
-  public static void run() {
+  public void run() {
     System.out.println("Codigo rodando em outra thread!");
   }
 }
 
-//OU IMPLEMENTANDO A INTERFACE Runnable
+// OU IMPLEMENTANDO A INTERFACE Runnable
 /**
  * public class Main implements Runnable {
-  public static void main(String[] args) {
-    Main obj = new Main();
-    Thread thread = new Thread(obj);
-    thread.start();
-    System.out.println("This code is outside of the thread");
-  }
-  public void run() {
-    System.out.println("This code is running in a thread");
-  }
-}
+ * public static void main(String[] args) {
+ * Main obj = new Main();
+ * Thread thread = new Thread(obj);
+ * thread.start();
+ * System.out.println("This code is outside of the thread");
+ * }
+ * public void run() {
+ * System.out.println("This code is running in a thread");
+ * }
+ * }
  */
