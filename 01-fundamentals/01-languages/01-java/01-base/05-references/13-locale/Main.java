2534bbe7dc76e7194c0c4c30fdcac19e6e081fce
GAMELEIRA <gameleira270499@gmail.com>
2025-02-12 21:24:09 -0300

    feat(locale): Adiciona aprendizado com locale para definir a entrada de dados com padrao especifico

--- 01-fundamentals/01-languages/01-java/01-base/05-references/13-locale/Main.java
+++ 01-fundamentals/01-languages/01-java/01-base/05-references/13-locale/Main.java
@@ -1,6 +1,11 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
+
+  //PARA DEFINIR QUE TODA ENTRADA IRA RESPEITAR OS PADROES DETERMINADOS PELO PAIS ESCOLHIDO
+  public static final Scanner scanner = new Scanner(System.in).useLocale(Locale.US); 
+
   public static void main(String[] args) {
     //FORCA O PROGRAMA A USAR OS PADROES DE UM PAIS EM ESPECIFICO PARA NUMEROS E AFINS
     Locale.setDefault(Locale.US);
