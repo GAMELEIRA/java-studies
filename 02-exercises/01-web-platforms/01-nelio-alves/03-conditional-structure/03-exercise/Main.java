01a812901c1628892a6cbe78638d3a5320db7cfa
GAMELEIRA <gameleira270499@gmail.com>
2025-02-14 13:55:49 -0300

    feat(conditional-03-exercise): Adiciona resolucao de exercicio basico sobre estrutura condicional que valida se uma lista de numeros sao multiplos

---
+++ 02-exercises/01-web-platforms/01-nelio-alves/03-conditional-structure/03-exercise/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+  public static Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+    for (int i = 0; i < 2; i++) {
+      numbers.add(requestNumber(i));
+    }
+
+    boolean areMultiple = areMultiple(numbers);
+    showResult(areMultiple);
+
+  }
+
+  public static int requestNumber(int sequence) {
+    int number;
+
+    System.out.printf("Informe o %d numero: %n", sequence);
+    number = scanner.nextInt();
+
+    return number;
+  }
+
+  public static boolean areMultiple(ArrayList<Integer> numbers) {
+    boolean areMultiple = false;
+
+    Collections.sort(numbers);
+    for (Integer number : numbers) {
+      for (Integer nextNumber : numbers) {
+        areMultiple = (number % nextNumber == 0);
+        if (areMultiple == false) {
+          break;
+        }
+      }
+    }
+
+    return areMultiple;
+  }
+
+  public static void showResult(boolean areMultiple) {
+    System.out.println(areMultiple ? "Sao Multiplos" : "Nao sao Multiplos");
+  }
+
+}
