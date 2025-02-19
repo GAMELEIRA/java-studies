1fc61171a386d85dd0b28c023c892ac81682dcd4
GAMELEIRA <gameleira270499@gmail.com>
2025-02-19 12:00:54 -0300

    feat(conditional-07-exercise): Adiciona resolucao de exercicio basico sobre estrutura condicional que valida as coordenadas de um plano cartesiano e retorna o quadrante baseado nos valores informados pelo usuario

---
+++ 02-exercises/01-web-platforms/01-nelio-alves/03-conditional-structure/07-exercise/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+  public static Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+  public static void main(String[] args) {
+    ArrayList<Double> data = getData();
+    String coordinates = getCoordinates(data);
+    printResult(coordinates);
+  }
+
+  public static ArrayList<Double> getData() {
+    ArrayList<Double> data = new ArrayList<Double>();
+
+    for (int i = 0; i < 2; i++) {
+      System.out.printf("Informe o %d valor: ", i);
+      Double value = scanner.nextDouble();
+      data.add(value);
+    }
+
+    return data;
+  }
+
+  public static String getCoordinates(ArrayList<Double> data) {
+    if (data.get(0) == 0 && data.get(1) == 0) {
+      return "Origem";
+    } else if (data.get(0) > 0 && data.get(1) > 0) {
+      return "Q1";
+    } else if (data.get(0) > 0 && data.get(1) < 0) {
+      return "Q4";
+    } else if (data.get(0) < 0 && data.get(1) < 0) {
+      return "Q3";
+    } else {
+      return "Q2";
+    }
+  }
+
+  public static void printResult(String coordinates) {
+    System.out.println(coordinates);
+  }
+
+}
