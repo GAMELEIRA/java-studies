229767411429ba99ab8ce96558ccc5782caee2d6
GAMELEIRA <gameleira270499@gmail.com>
2025-02-19 16:44:49 -0300

    feat(conditional-08-exercise): Adiciona resolucao de exercicio basico sobre estrutura condicional que informa a quantidade de taxa que uma pessoa deve pagar de imposto baseado no seu salario

---
+++ 02-exercises/01-web-platforms/01-nelio-alves/03-conditional-structure/08-exercise/Main.java
@@ -0,0 +1,43 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    public static void main(String[] args) {
+        double salary = getSalary();
+        double taxAmount = getTax(salary);
+        showResult(taxAmount);
+    }
+
+    public static double getSalary() {
+        System.out.println("Informe o salario: ");
+        return scanner.nextDouble();
+    }
+
+    public static double getTax(double salary) {
+        double tax = 0.0;
+
+        if (salary > 4500.00) {
+            tax += (salary - 4500.00) * 0.28;
+            salary = 4500.00;
+        }
+        if (salary > 3000.00) {
+            tax += (salary - 3000.00) * 0.18;
+            salary = 3000.00;
+        }
+        if (salary > 2000.00) {
+            tax += (salary - 2000.00) * 0.08;
+        }
+
+        return tax;
+    }
+
+    public static void showResult(double taxAmount) {
+        if (taxAmount == 0.0) {
+            System.out.println("Isento");
+        } else {
+            System.out.printf("R$ %.2f%n", taxAmount);
+        }
+    }
+}
