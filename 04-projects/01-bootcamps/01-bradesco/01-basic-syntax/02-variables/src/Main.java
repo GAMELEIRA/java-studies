9d4ca5b3c24c8dd5d9dfd00fe09d22cc0a009f83
GAMELEIRA <gameleira270499@gmail.com>
2025-03-06 15:00:26 -0300

    feat(variables): Adiciona aprendizado inicial com declaracao de variaveis em Java

---
+++ 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/02-variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //ESTRUTURA PARA DECLARACAO DE VARIAVEIS
+        //TIPO IDENTIFICADOR = ATRIBUICAO
+        String name = "Gabriel";
+        double height = 1.80;
+        int age = 25;
+        boolean isMaried = true;
+    }
+}
