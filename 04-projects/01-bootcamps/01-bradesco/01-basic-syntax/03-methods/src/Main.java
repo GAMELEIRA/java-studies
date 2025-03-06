d8af411b1bf9f93245df6e0a2bd5a8931536d4d5
GAMELEIRA <gameleira270499@gmail.com>
2025-03-06 15:21:14 -0300

    feat(methods): Adiciona aprendizado com declaracao e estrutura de metodos

---
+++ 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/03-methods/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(sum(1, 2));
+    }
+
+    // ESTRUTURA PARA DECLARACAO DE METODOS
+    // DECLARAR SEMPRE OS NOMES DOS METODOS NO INFINITIVO (FORMA BASICA DE UM VERBO,
+    // SEM FLEXAO DE TEMPO, MODO, NUMERO OU PESSOA), SEMPRE INDICANDO UMA ACAO
+    // ENCAPSULAMENTO RETORNO IDENTIFICADOR PARAMETROS
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+}
