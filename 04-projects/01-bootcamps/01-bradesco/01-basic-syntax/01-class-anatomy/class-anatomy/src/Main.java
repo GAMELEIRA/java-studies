f38bcaf5478b8a0d32bfed718cf31ea1134534e4
GAMELEIRA <gameleira270499@gmail.com>
2025-03-06 14:41:30 -0300

    feat(class-anatomy): Adiciona aprendizado com anatomia inicial de um programa em Java

---
+++ 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/01-class-anatomy/class-anatomy/src/Main.java
@@ -0,0 +1,7 @@
+//DECLARAR SEMPRE CLASSES COM O PADRAO CAMEL CASE
+public class Main {
+    //METODO ESPECIAL DE EXECUCAO PARA INSTANCIAR A APLICACAO
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
