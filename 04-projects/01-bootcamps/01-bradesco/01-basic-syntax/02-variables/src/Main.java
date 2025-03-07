7aad4e875cb21db6f31ca87c9ca40fb92638a879
GAMELEIRA <gameleira270499@gmail.com>
2025-03-07 13:28:33 -0300

    feat(type-variables): Adiciona aprendizado com tipo de variaveis em Java

--- 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/02-variables/src/Main.java
+++ 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/02-variables/src/Main.java
@@ -6,5 +6,18 @@ public class Main {
         double height = 1.80;
         int age = 25;
         boolean isMaried = true;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 1212324324344L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        //ALTERANDO O VALOR DE UMA VARIAVEL
+        name = "Cibele";
+
+        //DECLARACAO DE VARIAVEL CONSTANTE
+        //SEMPRE ESCREVER EM CAIXA ALTA
+        final int PI = 3.14;
     }
 }
