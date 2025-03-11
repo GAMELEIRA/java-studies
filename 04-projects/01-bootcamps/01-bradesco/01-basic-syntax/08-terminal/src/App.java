a57d85a8e755d510788df22277efac6c50046f21
GAMELEIRA <gameleira270499@gmail.com>
2025-03-11 15:37:27 -0300

    feat(terminal): Adiciona aprendizado de como visualizar no terminal o bytecode gerado no java

---
+++ 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/08-terminal/src/App.java
@@ -0,0 +1,7 @@
+//PARA VER UM BYTECODE EM LINGUAGEM INTERMEDIRIA EXECUTE java -c App.class
+//.class E O BYTECODE GERADO PELO javac
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
