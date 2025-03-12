983ebd3e42dda23b02c2e79f475981cf058c54d1
GAMELEIRA <gameleira270499@gmail.com>
2025-03-12 11:47:29 -0300

    feat(scanner): Adiciona aprendizado com Scanner em Java

--- 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/08-terminal/src/App.java
+++ 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/08-terminal/src/App.java
@@ -1,5 +1,8 @@
 //PARA VER UM BYTECODE EM LINGUAGEM INTERMEDIRIA EXECUTE java -c App.class
 //.class E O BYTECODE GERADO PELO javac
+
+import java.util.Scanner;
+
 public class App {
     //É POSSIVEL PASSAR ARGUMENTOS PARA O args NO MOMENTO DA EXECUCAO DO PROJETO java <nome do arquivo> <argumentos>
     public static void main(String[] args) throws Exception {
@@ -14,6 +17,14 @@ public class App {
             surname = "Sobrenome nao informado";
         }
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe sua idade");
+        int age = sc.nextInt();
+
         System.out.println("Meu nome: " + name + " " + surname);
+        System.out.println("Sua idade: " + age);
+
+        sc.close();
     }
 }
