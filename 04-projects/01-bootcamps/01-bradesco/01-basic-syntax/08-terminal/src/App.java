297ca9c20fca94569245c0281a1b485ec1d99532
GAMELEIRA <gameleira270499@gmail.com>
2025-03-12 11:31:29 -0300

    feat(args): Adiciona aprendizado de como utilizar os argumentos que sao passados para o parametro args

--- 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/08-terminal/src/App.java
+++ 04-projects/01-bootcamps/01-bradesco/01-basic-syntax/08-terminal/src/App.java
@@ -1,7 +1,19 @@
 //PARA VER UM BYTECODE EM LINGUAGEM INTERMEDIRIA EXECUTE java -c App.class
 //.class E O BYTECODE GERADO PELO javac
 public class App {
+    //É POSSIVEL PASSAR ARGUMENTOS PARA O args NO MOMENTO DA EXECUCAO DO PROJETO java <nome do arquivo> <argumentos>
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String name; 
+        String surname; 
+
+        try {
+            name = args[0];
+            surname = args[1];
+        } catch (Exception e) {
+            name = "Nome nao informado";
+            surname = "Sobrenome nao informado";
+        }
+
+        System.out.println("Meu nome: " + name + " " + surname);
     }
 }
