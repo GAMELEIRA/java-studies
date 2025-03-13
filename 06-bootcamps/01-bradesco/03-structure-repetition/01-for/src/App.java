d3ba7e818fe5c49633f9f15b3bd9de8493c47bdf
GAMELEIRA <gameleira270499@gmail.com>
2025-03-13 16:12:46 -0300

    feat(break-continue): Adiciona aprendizado com o uso de break e continue na estrutura de repeticao for

--- 06-bootcamps/01-bradesco/03-structure-repetition/01-for/src/App.java
+++ 06-bootcamps/01-bradesco/03-structure-repetition/01-for/src/App.java
@@ -1,13 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    
+
         // ESTRUTURA DE REPETICAO FOR EXECUTA BLOCO DE CODIGO
-        //BASEANDO-SE EM UM CONDICAO ONDE UM CONTADOR É AUMENTADO OU DECREMENTADO 
+        // BASEANDO-SE EM UM CONDICAO ONDE UM CONTADOR É AUMENTADO OU DECREMENTADO
         for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
 
         for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (i == 2) {
+                break; // break É UTULIZADO PARA INTERROMPER UM LACO DE REPETICAO
+            }
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            if (i == 2) {
+                break; // continue É UTULIZADO PARA INTERROMPER A EXECUCAO ATUAL E SEGUIR PARA O PROXIMO LACO DE REPETICAO
+            }
             System.out.println(i);
         }
 
