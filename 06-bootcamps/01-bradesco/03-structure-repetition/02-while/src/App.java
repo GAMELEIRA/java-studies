7d531d487a2318224fa6a97dbb1138499af9f453
GAMELEIRA <gameleira270499@gmail.com>
2025-03-13 16:16:12 -0300

    feat(while): Adiciona aprendizado a estrutura de repeticao while

---
+++ 06-bootcamps/01-bradesco/03-structure-repetition/02-while/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //EXECUTA UM BLOCO DE CODIGO ENQUANTO UMA CONDICAO FOR VERDADEIRA
+        int i = 0; 
+
+        while (i < 10) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
