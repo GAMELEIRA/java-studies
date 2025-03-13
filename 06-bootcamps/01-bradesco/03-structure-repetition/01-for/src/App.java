c860158edcaf6a48e2f94891579c83313b693611
GAMELEIRA <gameleira270499@gmail.com>
2025-03-13 15:52:25 -0300

    feat(for): Adiciona aprendizado com a estrutura de repeticao for

---
+++ 06-bootcamps/01-bradesco/03-structure-repetition/01-for/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        // ESTRUTURA DE REPETICAO FOR EXECUTA BLOCO DE CODIGO
+        //BASEANDO-SE EM UM CONDICAO ONDE UM CONTADOR É AUMENTADO OU DECREMENTADO 
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
