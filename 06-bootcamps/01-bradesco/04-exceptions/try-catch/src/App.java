ca18423cd7e9007ad3e29424b2e7c54475b1ad5c
GAMELEIRA <gameleira270499@gmail.com>
2025-03-13 17:01:37 -0300

    feat(try-catch): Adiciona aprendizado com tratamento de exececoes em Java

--- 06-bootcamps/01-bradesco/04-exceptions/try-catch/src/App.java
+++ 06-bootcamps/01-bradesco/04-exceptions/try-catch/src/App.java
@@ -5,16 +5,22 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         int number;
 
-        //try catch PERMITE QUE VOCE CONTROLE UM BLOCO DE CODIGO PARA QUE ELE CONSIGA SER RESILIENTE A ERROS
+        // try catch PERMITE QUE VOCE CONTROLE UM BLOCO DE CODIGO PARA QUE ELE CONSIGA
+        // SER RESILIENTE A ERROS
+        // try PERMITE QUE VOCE DEFINA UM BLOCO DE CODIGO A SER TESTADO
         try {
             System.out.println("Informe um valor numerico: ");
             number = scanner.nextInt();
-    
+
             System.out.println("Numero lido: " + number);
+            // catch PERMITE DEFINIR UM BLOCO DE CODIGO A SER EXECUTADO CASO OCORRA UM ERRO
+            // NO try
         } catch (Exception e) {
             System.out.println("Nao foi possivel ler o numero informado");
+            // finally EXECUTA UM BLOCO DE CODIGO APOS A CONCLUSAO DO BLOCO try ou catch
+        } finally {
+            System.out.println("FINALIZANDO O PROGRAMA");
         }
-
 
         scanner.close();
     }
