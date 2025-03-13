94421609c626b285e46de81f0c53da3a0b629a0e
GAMELEIRA <gameleira270499@gmail.com>
2025-03-13 14:30:25 -0300

    feat(composting-conditional): Adiciona aprendizado com estrutura condicionais compostas

---
+++ 06-bootcamps/01-bradesco/02-structure-conditional/02-composting-conditional/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int score = 6;
+        
+        //ESTRUTURA CONDICIONAL COMPOSTA SE BASEA EM EXECUTAR UM BLOCO DE CODIGO BASEANDO-SE EM UMA CONDICAO VERDADEIRA OU FALSA
+        if (score >= 7) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+   
+    }
+}
