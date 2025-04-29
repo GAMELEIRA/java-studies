e832352526f62dedf8c34501d2c281d843fcc1b6
GAMELEIRA <gameleira270499@gmail.com>
2025-04-29 16:49:28 -0300

    feat(consomer): Adiciona exemplo conceitual de como utilizar o consumer em java

---
+++ 01-fundamentals/02-concepts/01-oop/07-other-resources/07-consumer/01-examples/01-oders-system/src/org/gameleira/app/App.java
@@ -0,0 +1,23 @@
+package org.gameleira.app;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+import org.gameleira.models.Client;
+import org.gameleira.models.Order;
+
+public class App {
+
+	public static void main(String[] args) {
+		ArrayList<Order> orders = new ArrayList();
+		orders.add(new Order(1, new Client("Gabriel"), 100.00));
+		processOrders(orders);
+
+	}
+	
+	public static void processOrders(ArrayList<Order> orders) {
+		Consumer<Order> consumerOrders = order -> System.out.println(order.toString());
+		orders.forEach(consumerOrders);
+	}
+
+}
